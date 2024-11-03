fa048c415c6440aa03340d145aaac0e8824790c6
Omar Elmasry <74859926+Omar-Elmassry@users.noreply.github.com>
2024-11-03 16:10:45 +0200

    edits

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -45,7 +45,7 @@ function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-a100 bg-red-500 py-6 flex flex-col justify-center sm:py-12">
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
