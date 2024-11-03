cae8f9d6cca7428c747b37da829316b39c1d1748
Omar Elmasry <74859926+Omar-Elmassry@users.noreply.github.com>
2024-11-03 16:17:44 +0200

    add shadcn config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -12,12 +13,12 @@ export default defineConfig({
         short_name: "IshaTime",
         icons: [
           {
-            src: "/icons/icon-192x192.png",
+            src: "/icons/192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "/icons/512.png",
             sizes: "512x512",
             type: "image/png",
           },
@@ -26,4 +27,9 @@ export default defineConfig({
     }),
   ],
   base: "/isha-time/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
