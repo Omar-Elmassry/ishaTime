3905c9df96113bebfeab8bcfe364f6eb07045f2e
Omar Elmasry <74859926+Omar-Elmassry@users.noreply.github.com>
2025-01-25 00:51:44 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/isha-time/sw.js', { scope: '/isha-time/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ishaTime/sw.js', { scope: '/ishaTime/' })})}
